2028ab43966bda25bc10793cfe218ce5c71b26c5
Divyanshu Tiwari <tdivyanshu510@gmail.com>
2021-06-30 14:54:12 +0530

    Added rules and credits

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class MainMenu(Screen):
     # This method will toggle the music of the game.
     def toggle_music(self):
         global to_play_music
-        Sound.volume = 1 if Sound.volume == 0 else 0  # toggle mute the music by changing volume level
+        Sound.volume = 1 if Sound.volume == 0 else 0  # toggles the music by changing volume level
         bg_music.stop() if bg_music.state == "play" else bg_music.play()  # toggles state of background music
         # Change the source of image for representing the current state of music
         if self.ids.audio_switch_btn.source == "Images/audio_on.png":
@@ -60,15 +60,13 @@ class EnterName(Screen):
         global name_of_user
         # if user has entered any name, store it else name will be 'You'
         name_of_user = self.ids.username.text.title() if self.ids.username.text != "" else 'You'
-        # 'ids' will become referencable when 'main_game' will be set to 'MainGame' screen in build() method of
-        # 'RockPaperScissorApp' class
-        main_game.ids.user_name.text = name_of_user
-        main_game.name_of_user = self.ids.username.text.upper()
+        main_game.ids.user_name.text = name_of_user  # set name of user in game
+        # Stop music and remove screen switch transition
         bg_music.stop()
         main_game.unmute_music()
         screen_manager.transition = NoTransition()
 
-        def change_screen(delta_time):
+        def change_screen(delta_time):  # function to change screen from EnterName to MainGame
             self.ids.username.text = ""
             screen_manager.current = "Main Game"
             if to_play_music:
@@ -78,6 +76,7 @@ class EnterName(Screen):
         def start_game(delta_time):
             main_game.start_round()
 
+        # Start the game after 2 seconds
         Clock.schedule_once(change_screen, 2)
         Clock.schedule_once(start_game, 2)
 
@@ -93,7 +92,7 @@ class BlankScreen(Screen):
     pass
 
 
-# This screen appears after 2 seconds when 'BlankScreen' appears on 'Start' button click of 'EnterName' screen
+# This screen appears after 2 seconds when 'BlankScreen' appears on click of 'Start' button of 'EnterName' screen
 # This is the screen where the game will be played
 class MainGame(Screen):
     click_sound = SoundLoader.load("Sounds/click.ogg")
@@ -136,20 +135,23 @@ class MainGame(Screen):
     schedule_timings = {"rst": 2, "rsp": 1.7}  # rst: round_start_time, rsp: round_sound_play
     game_playable = True
 
+    # Cheats related variables
     always_win = False
     always_lose = False
     clicks = 0
 
     def show_exit_prompt(self):
         if self.game_playable:
+            # Lower the opacity of all the widgets
             self.ids.background_image.opacity = 0.4
             self.ids.switch_to_home.opacity = 0.4
             self.ids.game_actions.opacity = 0.4
             self.ids.pause_game.opacity = 0.4
+
             self.game_playable = False
             self.add_widget(ExitPrompt())
 
-    def set_message(self, text, delta_time, set_computer_choice=False, choice=""):
+    def set_message(self, text: str, delta_time, set_computer_choice=False, choice=""):
         self.message.text = text
         if text == "It's your turn !!":
             self.user_choice_text.text = ""
@@ -173,7 +175,9 @@ class MainGame(Screen):
             self.select_scissor.disabled = True
             self.select_scissor.opacity = 0.7
 
-    def get_computer_choice(self, user_choice):
+    # This method will get the choice of computer. If any cheat is activated, choice will be based on that cheat,
+    # otherwise choice will be random.
+    def get_computer_choice(self, user_choice: str):
         choices = ["Rock", "Paper", "Scissor"]
         choices_for_win = {"Rock": "Scissor", "Paper": "Rock", "Scissor": "Paper"}
         choices_for_lose = {"Rock": "Paper", "Paper": "Scissor", "Scissor": "Rock"}
@@ -188,9 +192,11 @@ class MainGame(Screen):
     def set_computer_choice(self, choice):
         self.computer_choice_text.text = choice
 
+    # This method will compare the choices of player and computer and then return the winner
     def get_winner(self, choices: dict, delta_time):
         choices_list = list(choices.keys())
         self.winner = "TIE"
+        # Comparing choices
         if "Rock" in choices_list and "Paper" in choices_list:
             self.winner = choices["Paper"]
         elif "Paper" in choices_list and "Scissor" in choices_list:
@@ -198,6 +204,8 @@ class MainGame(Screen):
         elif "Scissor" in choices_list and "Rock" in choices_list:
             self.winner = choices["Rock"]
 
+        # Changing the appearance of text on game to represent the winner, playing sound effects, increasing points and
+        # increment round number
         if self.winner == "player":
             self.user_choice_text.color = 0, 1, 0
             self.computer_choice_text.color = 1, 0, 0
@@ -215,30 +223,34 @@ class MainGame(Screen):
         else:
             self.tie_sound.play()
 
-        Clock.schedule_once(self.start_round, 2)
+        Clock.schedule_once(self.start_round, 2)  # Start next round after 2 seconds
         return self.winner
 
     def set_round_text(self, delta_time):
         self.user_choice_text.color = 203/255, 158/255, 249/255
         self.computer_choice_text.color = 203/255, 158/255, 249/255
-        self.round_on_screen.opacity = 1
+        self.round_on_screen.opacity = 1  # change the opacity of round number to 1 which is 0 when the game starts
         if self.current_round < 6:
             self.round_on_screen.text = f"ROUND {self.current_round}"
         elif self.current_round == 6:
             self.round_on_screen.text = "FINAL ROUND"
         else:
             self.round_on_screen.text = "TIE BREAKER"
-        # self.round_on_screen.text = f"ROUND {self.current_round}" if self.current_round < 6 else "FINAL ROUND"
 
     def start_round(self, delta_time=0):
+        # Round will be played if the current_round is less than or equal to 6. And If the points of the user and the
+        # computer are equal, tie breaker round will be played
         if self.current_round <= 6 or self.points["user"] == self.points["computer"]:
+            # Play sound effect of current round and set the text of round in game
             Clock.schedule_once(lambda dt: self.round_sounds[self.current_round].play(), self.schedule_timings["rsp"])
             Clock.schedule_once(self.set_round_text, self.schedule_timings["rst"])
-            Clock.schedule_once(partial(self.set_message, "It's your turn !!"), self.schedule_timings["rst"])
+            # Enable the buttons and tell the user that it's his turn
             Clock.schedule_once(self.toggle_buttons_state, self.schedule_timings["rst"])
+            Clock.schedule_once(partial(self.set_message, "It's your turn !!"), self.schedule_timings["rst"])
+            # Scheduled timings will be changed after first round
             self.schedule_timings["rst"] = 0.3
             self.schedule_timings["rsp"] = 0
-        else:
+        else:  # After the sixth round or tie breaker round, tell the user who is the winner
             if self.points["user"] > self.points["computer"]:
                 self.winner_sound.play()
                 self.user_choice_text.text = "WINNER"
@@ -247,9 +259,12 @@ class MainGame(Screen):
                 self.loser_sound.play()
                 self.user_choice_text.text = "LOSER"
                 self.computer_choice_text.text = "WINNER"
+            # Enable and show the 'Play again' button
             self.play_again_button.disabled = False
             self.play_again_button.opacity = 1
 
+    # This method will get the user's choice and computer's choice, and then compare them to get the winner by using the
+    # get_winner() method
     def compare_choices(self, selection):
         options = {1: "Rock", 2: "Paper", 3: "Scissor"}
         user_choice = options[selection]
@@ -259,11 +274,13 @@ class MainGame(Screen):
         Clock.schedule_once(partial(self.set_message, "", True, choice=computer_choice), 1.5)
         Clock.schedule_once(partial(self.get_winner, {user_choice: "player", computer_choice: "computer"}), 2)
 
-    def play_again(self):
+    def play_again(self):  # method for 'Play again' button
         global main_game
         screen_manager.current = "Blank Screen"
+        # reset points
         self.points["user"] = 0
         self.points["computer"] = 0
+        # reset the game by removing current 'MainGame' screen and adding a new one
         screen_manager.remove_widget(main_game)
         main_game = MainGame(name="Main Game")
         main_game.user_name.text = name_of_user
@@ -271,6 +288,7 @@ class MainGame(Screen):
         screen_manager.current = "Main Game"
         Clock.schedule_once(main_game.start_round, 1)
 
+    # The two methods given below will set mute/unmute the music by changing their volume
     def mute_music(self):
         for sound in self.sounds_for_mute_unmute_stop:
             sound.volume = 0
@@ -284,6 +302,7 @@ class MainGame(Screen):
             for sound in self.round_sounds.values():
                 sound.volume = 1
 
+    # This method will stop the music by changing their play state
     def stop_music(self):
         if to_play_music:
             main_game_music.stop()
@@ -295,18 +314,23 @@ class MainGame(Screen):
                 if sound.state == "play":
                     sound.stop()
 
-    def pause_game(self):
-        self.stop_music()
-        self.mute_music()
-        screen_manager.transition = NoTransition()
-        screen_manager.current = "Pause Screen"
+    def pause_game(self):  # method for pausing the game
+        if self.game_playable:
+            self.stop_music()
+            self.mute_music()
+            screen_manager.transition = NoTransition()
+            screen_manager.current = "Pause Screen"
 
+    # This method will be used by reset_cheats() method to set the clicks variable to 0
     def reset_clicks(self, delta_time):
         self.clicks = 0
 
+    # This method will calculate the clicks of the user on the 'MainGame' screen. If the user clicks three times within
+    # 0.5 seconds, all the cheats will be deactivated. Otherwise, it will reset the 'clicks' variable by using the above
+    # method
     def reset_cheats(self):
         if self.clicks == 0:
-            Clock.schedule_once(self.reset_clicks, 0.5)
+            Clock.schedule_once(self.reset_clicks, 0.5)  # reset the clicks after 0.5 seconds
         self.clicks += 1
         if self.clicks == 3 and self.message.text == "It's your turn !!":
             self.always_win = False
@@ -329,6 +353,7 @@ class ExitPrompt(Screen):
         # Remove 'MainGame' screen from screen manager and add it again to reset the game
         screen_manager.remove_widget(main_game)
         main_game = MainGame(name='Main Game')
+        # Reset the points
         main_game.points["user"] = 0
         main_game.points["computer"] = 0
         screen_manager.add_widget(main_game)
@@ -338,16 +363,16 @@ class ExitPrompt(Screen):
         main_game.game_playable = True
 
     def remove_prompt(self):
-        # 'ids' will become referencable when 'main_game' will be set to 'MainGame' screen in build() method of
-        # 'RockPaperScissorApp' class
+        # Reset the opacity of widgets to make them visible
         main_game.ids.background_image.opacity = 0.8
         main_game.ids.switch_to_home.opacity = 1
         main_game.ids.game_actions.opacity = 1
         main_game.ids.pause_game.opacity = 1
-        main_game.remove_widget(self)
+        main_game.remove_widget(self)  # remove exit prompt from 'MainGame' screen
         main_game.game_playable = True
 
 
+# This screen will be shown when user pauses the game
 class PauseScreen(Screen):
     click_sound = SoundLoader.load("Sounds/click.ogg")
 
@@ -359,14 +384,17 @@ class PauseScreen(Screen):
         screen_manager.current = 'Main Game'
         screen_manager.transition = WipeTransition()
 
-    top_button = ObjectProperty(None)
-    bottom_button = ObjectProperty(None)
+    # Following code is related to cheats
+    top_button = ObjectProperty(None)  # hidden and disabled button
+    bottom_button = ObjectProperty(None)  # hidden button
 
     clicks = 0
 
+    # This method will be used by enable_top_button() and show_cheats() methods to set the clicks variable to 0
     def reset_clicks(self, delta_time):
         self.clicks = 0
 
+    # If user clicks on the hidden bottom_button three times within 0.5 seconds, this method will enable the top_button
     def enable_top_button(self):
         if self.clicks == 0:
             Clock.schedule_once(self.reset_clicks, 0.5)
@@ -375,6 +403,7 @@ class PauseScreen(Screen):
             self.top_button.disabled = False
             self.bottom_button.disabled = True
 
+    # After the top_button is enabled by above method, clicking it three times within 0.5 seconds will reveal the cheats
     def show_cheats(self):
         if self.clicks == 0:
             Clock.schedule_once(self.reset_clicks, 0.5)
@@ -385,6 +414,8 @@ class PauseScreen(Screen):
             screen_manager.current = "Cheats Screen"
 
 
+# Cheats can be activated on this screen. There's a secret way to access this screen - Pause the game, triple tap on the
+# bottom of the screen and then triple tap on the top of the screen; this will reveal this screen.
 class CheatsScreen(Screen):
     always_win_button = ObjectProperty(None)
     always_lose_button = ObjectProperty(None)
@@ -401,6 +432,17 @@ class CheatsScreen(Screen):
             main_game.always_lose = True
 
 
+# This screen includes all the rules of the game. This screen also provides the user a hint for accessing the cheats.
+class RulesScreen(Screen):
+    click_sound = SoundLoader.load("Sounds/click.ogg")
+
+
+# This screen includes the all the credits of the screen - The developer and all the websites which have provided the
+# assets required for the game
+class CreditsScreen(Screen):
+    click_sound = SoundLoader.load("Sounds/click.ogg")
+
+
 # This is the root widget of all the screens and widgets of the game
 class RockPaperScissorApp(App):
     def build(self):
@@ -410,6 +452,8 @@ class RockPaperScissorApp(App):
         screen_manager.add_widget(BlankScreen(name='Blank Screen'))
         main_game = MainGame(name='Main Game')
         screen_manager.add_widget(main_game)
+        screen_manager.add_widget(RulesScreen(name='Rules Screen'))
+        screen_manager.add_widget(CreditsScreen(name='Credits Screen'))
         screen_manager.add_widget(ExitPrompt(name='Exit Prompt'))
         screen_manager.add_widget(PauseScreen(name='Pause Screen'))
         cheats_screen = CheatsScreen(name='Cheats Screen')
